fb692402a03786981b03537c00f64aa3556f8e40
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-04-12 23:01:52 -0500

    fin de la expliacion sobre ngOnInit y ngOnDestroy

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrl: './price.component.css'
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
 
+  @Input()
+  public price: number = 0;
+
+  //cuando se maneja observable se pone un simbol de dolar
+  public interval$?: Subscription;
+
+  ngOnInit(): void {
+    console.log('OnInitPrice HIJO');
+    //interval es un observable que emite valores de manera secuencial basado en un periodo de tiempo
+    this.interval$ = interval(1000).subscribe(valor => {
+      console.log(valor)
+    });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('OnChangesPrice HIJO');
+    console.log({ changes });
+  }
+  ngOnDestroy(): void {
+    console.log('OnDestroyPrice HIJO');
+    //el unsubscribe va a cancelar al subcripscion
+    this.interval$?.unsubscribe()
+  }
 }
--- src/app/products/pages/product/product.component.ts
+++ src/app/products/pages/product/product.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  //si no se implementan los ciclos de vida no los usa nunca
+  public productoVisible: boolean = false;
+  public precioActual: number = 10;
 
+  constructor() {
+    console.log('constructor')
+  }
+  ngOnInit(): void {
+    console.log('OnInit')
+  }
+
+  //ng on changes solo escucha inputs
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+    console.log('OnChanges')
+  }
+  ngDoCheck(): void {
+    console.log('DoCheck')
+  }
+  //cuando el contenido se inicializado
+  ngAfterContentInit(): void {
+    console.log('AfterConatntInit')
+  }
+  //cuando se verifica que se inicializo
+  ngAfterContentChecked(): void {
+    console.log('AfterContentChecked')
+  }
+  //cuando ya se realizo las modificaciones
+  ngAfterViewInit(): void {
+    console.log('AfterViewInit')
+  }
+  //verificar si las modificaciones
+  ngAfterViewChecked(): void {
+    console.log('AfterViewChecked')
+  }
+  //hay que manejarlo bien porque nosotros cuando no limpiamos nuestro despues de hacer algo
+  //podemos tener problemas de memoria o algun proceso se dispara dos veces
+  //
+  ngOnDestroy(): void {
+    console.log('OnDestroy')
+  }
+
+  incrementarPrecio() {
+    this.precioActual++;
+  }
 }
